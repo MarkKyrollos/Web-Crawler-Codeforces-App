5f805b8747171054156730ac848c4c994d8c242e
Mark George Kyrollos <98829362+MarkKyrollos@users.noreply.github.com>
2024-12-12 16:05:01 +0200

    Delete Contests.sql

--- Contests.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Contests (
-    contest_id INT AUTO_INCREMENT PRIMARY KEY,
-    contest_name VARCHAR(255) NOT NULL,
-    contest_date DATE NOT NULL,
-    division ENUM('Div.1', 'Div.2') NOT NULL,
-    writer VARCHAR(255),
-    problem_count INT NOT NULL
-);
